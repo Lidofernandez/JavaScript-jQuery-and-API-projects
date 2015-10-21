3b4b523492424cd778368f1e67a6cec67fd108b6
Lido Fernandez <lidofernandezburgos@gmail.com>
2015-10-21 03:25:50 +0200

    added buttons and modification of the AJAX callBacks

--- Custom_Search_Engine_with_Google_API/js/app.js
+++ Custom_Search_Engine_with_Google_API/js/app.js
@@ -1,138 +1,117 @@
 "use strict";
 
-/*Getting the Google API.......................................................................................*/
+function sendAjax (event) {
+	if (document.getElementById("search").value !== "") {
+		body(url(), printResults, numberOfPages);
+	} else {
+		console.log("Please type a term for search");
+	}
+	event.preventDefault();
+};
+var submitButton = document.getElementById("submit");
+submitButton.addEventListener('click', sendAjax, false);
 
-function APIurl (index) {
+function body (url, printResults, numberOfPages) {
+	var hrx = new XMLHttpRequest();
+		hrx.onreadystatechange = function () {
+			if (hrx.readyState === 4 ) {
+				if (hrx.status === 200 ) {				
+					var dataLinks = JSON.parse(hrx.responseText); //
+					printResults(dataLinks);
+					numberOfPages(dataLinks);
+					console.log(dataLinks);
+				} else {
+					console.log("HTTP error "+hrx.status+" "+hrx.statusText);
+				}
+			} else {
+				console.log(hrx.readyState);
+			}
+		};
+		hrx.open('GET', url, true);
+		console.log(url);
+		hrx.send();
+};
 
+function footer (url, printResults) {
+	var hrxFooter = new XMLHttpRequest();
+		hrxFooter.onreadystatechange = function () {
+			if (hrxFooter.readyState === 4 ) {
+				if (hrxFooter.status === 200 ) {				
+					var dataLinks = JSON.parse(hrxFooter.responseText); //
+					printResults(dataLinks);
+					console.log(dataLinks);
+				} else {
+					console.log("HTTP error "+hrxFooter.status+" "+hrxFooter.statusText);
+				}
+			} else {
+				console.log(hrxFooter.readyState);
+			}
+		};
+		hrxFooter.open('GET', url, true);
+		console.log(url);
+		hrxFooter.send();
+};
+
+function url (index) {
 	/*......................................................*/
-	// var apiKey = ""; 
-	// var searchGoogleID = "";
+	var apiKey = "AIzaSyCq2i7QqN9cXepXLsCqPTYfzvCJyo8Scd0"; 
+	var searchGoogleID = "004975204272958123692:mhgx86zgpoa";
 	/*......................................................*/
-
 	var searchInput = document.getElementById("search").value;
-	var numberOfPages = 10;//select the number of pages you want to see
-	var requestPageIndex = index;
 	var googleUrl = "https://www.googleapis.com/customsearch/v1?";
 	googleUrl += "key=" + apiKey;
 	googleUrl += "&cx=" + searchGoogleID;
 	googleUrl += "&q=" + searchInput;
-	googleUrl += "&num=" + numberOfPages;
-	googleUrl += "&start=" + requestPageIndex;
+	if (index !== undefined) {
+		googleUrl += "&start=" + index;
+	}
 	return googleUrl;
 };
 
-/*AJAX part.....................................................................................................*/
-
-function AJAXrequest (url) {
-	var hrx = new XMLHttpRequest();
-	hrx.onreadystatechange = function () {
-		if (hrx.readyState === 4 ) {
-			if (hrx.status === 200 ) {				
-				var dataLinks = JSON.parse(hrx.responseText); //
-				console.log(dataLinks);
-
-				/*Checking results.......................................................................*/
-
-				printWebResults(dataLinks);
-
-			} else {
-				// message = "HTTP error "+hrx.status+" "+hrx.statusText;
-				// print(message); in process
-				console.log("HTTP error "+hrx.status+" "+hrx.statusText);
-			}
-		}
-	};
-
-	/*Send and GET resquest ............................................................................*/
-
-	console.log(url);
-	hrx.open('GET', url, true);
-	hrx.send();
-};
-
-/*Getting Google Results......................................................................................*/
-
-function googleSearch (event) {
-	if (document.getElementById("search").value !== "") {
-		AJAXrequest(APIurl(1));
-
-	/*Errors messages for the search Box...................................................................*/
-
-	} else {
-		message = "<p>Please type a term for searching</p>";
-		print(message);
-
-	}
-	event.preventDefault();
-};
-
-/*Printer for error messages..............................*/
-
-function print(message) {
-
-	document.getElementById("message").innerHTML = message;
-	return message;
-};
-
-/*Printer for images and links....................................*/
-
-function printWebResults(dataLinks) {
-var totalResults = dataLinks.queries;
-if ( parseInt(totalResults.request[0].totalResults) > 0)	{
-	console.log(parseInt(totalResults.request[0].totalResults));
+function printResults (dataLinks) {
 	var printLinks = "<h2>Web results</h2>";
 		printLinks += '<ul class="links">';
 	for (var i = 0; i < dataLinks.items.length; i += 1) {
 		printLinks += '<div class="formatLinks">';
-		printLinks += '<li class="tittleLink">' + dataLinks.items[i].htmlTitle + '</li>';
-		printLinks += '<li><a href="' + dataLinks.items[i].link + '">' + dataLinks.items[i].link + '</a></li>';
+		printLinks += '<li class="tittleLink">';
+		printLinks += dataLinks.items[i].htmlTitle + '</li>';
+		printLinks += '<li><a href="' + dataLinks.items[i].link + '">'; 
+		printLinks += dataLinks.items[i].link + '</a></li>';
 		if (dataLinks.items[i].pagemap) {
-		printLinks += '<table class="description"><tr><td><a href="'+ dataLinks.items[i].link +'"><img src="'+ dataLinks.items[i].pagemap.cse_image[0].src+'" alt = "imgSection"></a></td>';
+		printLinks += '<table class="description"><tr><td><a href="';
+		printLinks += dataLinks.items[i].link;
+		printLinks += '"><img src="';
+		printLinks += dataLinks.items[i].pagemap.cse_image[0].src;
+		printLinks += '" alt = "imgSection"></a></td>';
 		}
-		printLinks += '<td><p>' + dataLinks.items[i].snippet + '</p></td></tr></table>';
+		printLinks += '<td><p>' + dataLinks.items[i].snippet;
+		printLinks += '</p></td></tr></table>';
 		printLinks += '</div>';
 	}
-	printLinks += '</ul>';
-	
+		printLinks += '</ul>';
 	document.getElementById("displayWebResults").innerHTML = printLinks;
-
-	
-	if (parseInt(totalResults.request[0].totalResults) > 0) {
-		var pages = Math.round(parseInt(totalResults.request[0].totalResults)/10);
-		console.log(pages);
-		var displayPages = document.getElementById("displayPages");
-		var printNavigation = "<ul>";
-		if (pages > 10) {
-
-			//create elements with javaScript http://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_document_createelement2
-
-			function displayPagesList (i) {
-				printNavigation += '<li><button type="button" ';
-				printNavigation += 'onclick= "' + AJAXrequest.bind(null, APIurl(i*10+1)) + ';" ';
-				printNavigation += ' >'+ parseInt(i) +'</button</li>';
-			};
-			for (var i = 1; i <= 10; i+=1) {
-				displayPagesList(i);
-			}
-		} else if (pages <= 10) {
-			for (var i = 2; i <= pages; i+=1) {
-				printNavigation += '<li><button type="button" '+ displayPages.addEventListener('click', AJAXrequest.bind(null, APIurl(i*10+1)), false) +' ">'+ parseInt(i) +'</button</li>';
-			}
-		}
-	}
-	printNavigation += '</ul>';
-	document.getElementById('displayPages').innerHTML = printNavigation; 
-
-/*Describing errors information........................................................*/	
-
-} else {
-	message = "<p>Ups... the following terms were not found: </p>" + document.getElementById("search").value;
-	print(message);
-} 
 
 };
 
-/*Handlers.................................................*/
-
-var startSearch = document.getElementById("submit");
-startSearch.addEventListener('click', googleSearch, false);
+function numberOfPages (dataLinks) {
+	if (parseInt(dataLinks.queries.request[0].totalResults) > 0) {
+		var displayPages = document.getElementById('displayPages');
+		var pages = Math.round(parseInt(dataLinks.queries.request[0].totalResults)/10);
+		console.log(pages);
+		if (pages > 10) {
+			function add (i) {
+			    var element = document.createElement("button");
+			    element.setAttribute("type", "button");
+			    element.innerHTML = i + 1;
+			    element.onclick = function() {
+			        footer(url(i*10+1), printResults);
+			    } 
+			    var foo = document.getElementById("displayPages");
+			    foo.appendChild(element);
+			}
+			for (var i = 0; i <= 9; i+=1) {
+				add(i);
+			}
+		}
+	}
+};
